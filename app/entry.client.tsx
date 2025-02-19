7b0c9e6ba0fc6eea9885cdd10bea78e74121e9f0
Yosuf Nawed <yosuf.nawed@example.com>
2025-02-18 22:00:38 -0600

    Update client entry point and build configuration

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,30 @@
 import { RemixBrowser } from '@remix-run/react';
-import { startTransition } from 'react';
+import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
-startTransition(() => {
-  hydrateRoot(document.getElementById('root')!, <RemixBrowser />);
-});
+function hydrate() {
+  startTransition(() => {
+    const root = document.getElementById('root');
+
+    if (!root) {
+      throw new Error('Root element not found. The application cannot be initialized.');
+    }
+
+    hydrateRoot(
+      root,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    );
+  });
+}
+
+if (typeof requestIdleCallback === 'function') {
+  requestIdleCallback(hydrate);
+} else {
+  /*
+   * Safari doesn't support requestIdleCallback
+   * https://caniuse.com/requestidlecallback
+   */
+  setTimeout(hydrate, 1);
+}
